f8f01ce1b93ccd419cec4d425222546db4aacda2
francois-roseberry <f_roseberry@live.fr>
2016-11-13 17:38:22 -0500

    Initial commit

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+node server/server.js -w client/
---
+++ server/server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var program = require('commander');
+
+var port = 3000;
+
+program
+	.version('0.1')
+	.option('-w, --webclient <directory>', 'The directory served')
+	.parse(process.argv);
+
+app.use(express.static(program.webclient));
+
+io.on('connection', function(socket) {
+	var address = socket.handshake.address;
+	console.log('A user connected from ' + address.address + ':' + address.port);
+	socket.on('disconnect', function() {
+		console.log('user disconnected');
+	});
+});
+
+http.listen(port, function () {
+	console.log('Question game server listening on port ' + port);
+});
