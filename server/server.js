ef6d7e54c0b3e2cd5c314b33829a40eca88d3d68
François Roseberry <f_roseberry@live.fr>
2016-12-06 10:29:15 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -15,14 +15,15 @@ app.use(express.static(program.webclient));
 
 io.on('connection', function(socket) {
 	// TODO : if there's currently a game when the user connects, should return a message and close the socket
-	// TODO : Save the client and his name in an array of players
+	// TODO : Save the client socket id and his name in an array of players, later will also contain the score
 	
 	var address = socket.handshake.address;
 	console.log('A user connected from ' + address.address + ':' + address.port);
 	socket.on('name', function (name) {
 		console.log('A user identified as [' + name + "], name ok");
 		// TODO : validate the name is unique
-		// TODO : the message should only be sent to this particular client, not everyone
+		// TODO : the message should only be sent to this particular client, not everyone,
+		// using something like io.sockets.socket(player.socketId).emit('name response', true)
 		socket.emit('name response', true);
 	});
 	
