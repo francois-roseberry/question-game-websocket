d6a8926fdb6b3d0e054e2b426d45901363d3ef09
francois-roseberry <f_roseberry@live.fr>
2017-05-09 16:11:58 -0400

    Replaced console.log() calls with a logger

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ var io = require('socket.io')(http);
 var program = require('commander');
 var _ = require('underscore');
 var fs = require('fs');
+var log = require('debug')('question-game');
 
 var PORT = 3000;
 
@@ -31,13 +32,13 @@ program
 	.option('-w, --webclient <directory>', 'The directory served')
 	.option('-q, --question <file>', 'The question file')
 	.parse(process.argv);
-	
+
 fs.readFile(program.question, 'utf-8', function (err, data) {
 	if (err) {
-		console.log('Could not read question file');
+		log('Could not read question file');
 		return;
 	}
-	
+
 	app.use(express.static(program.webclient));
 	io.on('connection', onConnect(JSON.parse(data)));
 });
@@ -56,19 +57,19 @@ function onConnect(questions) {
 function onPlayerName(socket) {
 	return function (name) {
 		if (gameStarted) {
-			console.log('A user cannot join because game is aleady started');
+			log('A user cannot join because game is aleady started');
 			socket.emit('name response', false, 'ALREADY_STARTED');
 			return;
 		}
-		
+
 		var names = _.map(players, function (player) {
 			return player.name;
 		});
 		if (_.contains(names, name)) {
-			console.log('A user cannot join because name (' + name + ') already exists');
+			log('A user cannot join because name (' + name + ') already exists');
 			socket.emit('name response', false, 'EXISTING');
 		} else {
-			console.log('A user identified as [' + name + "]");
+			log('A user identified as [' + name + "]");
 			players[socket.id] = {
 				name: name,
 				score: 0,
@@ -87,14 +88,14 @@ function onPlayerName(socket) {
 function onStart(socket, questions) {
 	return function () {
 		if (!players[socket.id]) {
-			console.log('Game cannot be started by a player who is not logged in');
+			log('Game cannot be started by a player who is not logged in');
 			return;
 		}
-		
-		console.log('Game started by [' + players[socket.id].name + ']');
+
+		log('Game started by [' + players[socket.id].name + ']');
 		countdown(countdownObject, SECONDS_BEFORE_START, function () {
 			gameStarted = true;
-			console.log('Game start, sending first question');
+			log('Game start, sending first question');
 			io.emit('question', questions[0].question, 1, questions.length);
 		});
 	};
@@ -103,15 +104,15 @@ function onStart(socket, questions) {
 function onCancel(socket) {
 	return function () {
 		if (!players[socket.id]) {
-			console.log('Game cannot be cancelled by a user who is not logged in');
+			log('Game cannot be cancelled by a user who is not logged in');
 			return
 		}
-		
+
 		if (countdownObject.timer) {
-			console.log('Game start cancelled by [' + players[socket.id].name + ']');
+			log('Game start cancelled by [' + players[socket.id].name + ']');
 			clearTimeout(countdownObject.timer);
 			countdownObject.timer = null;
-			
+
 			var names = _.map(players, function (player) {
 				return player.name;
 			});
@@ -123,21 +124,21 @@ function onCancel(socket) {
 function onAnswer(socket, questions) {
 	return function (answer) {
 		if (!players[socket.id]) {
-			console.log('Question cannot be answered by a user who is not logged in');
+			log('Question cannot be answered by a user who is not logged in');
 			return;
 		}
-		
+
 		var truth = questions[questionIndex].answer;
 		if (answer === truth) {
 			socket.emit('answer response', false, 'TRUTH');
 		} else {
-			console.log('Player [' + players[socket.id].name + '] has answered ' + answer);
+			log('Player [' + players[socket.id].name + '] has answered ' + answer);
 			players[socket.id].lastAnswer = answer;
 			socket.emit('answer response', true);
 			if (hasEveryPlayerAnswered()) {
 				var choices = computeChoices(truth);
 				shuffle(choices);
-				console.log('Everybody has answered, sending choices : ' + JSON.stringify(choices));
+				log('Everybody has answered, sending choices : ' + JSON.stringify(choices));
 				io.emit('choices', choices);
 			}
 		}
@@ -147,23 +148,23 @@ function onAnswer(socket, questions) {
 function onChoice(socket, questions) {
 	return function (choice) {
 		if (!players[socket.id]) {
-			console.log('A choice cannot be made by a player who is not logged in');
+			log('A choice cannot be made by a player who is not logged in');
 			return;
 		}
-		
-		console.log('Player [' + players[socket.id].name + '] has choosen ' + choice);
+
+		log('Player [' + players[socket.id].name + '] has choosen ' + choice);
 		players[socket.id].lastChoice = choice;
 		if (hasEveryPlayerChosen()) {
-			console.log('Everybody has chosen, computing scores');
-			
+			log('Everybody has chosen, computing scores');
+
 			var truth = questions[questionIndex].answer;
 			var resultsMap = {};
 			resultsMap[truth] = { authors: ['TRUTH'], choosedBy: []};
-			
+
 			_.each(players, function (player) {
 				resultsMap[player.lastAnswer] = getResult(resultsMap, player);
 			});
-			
+
 			_.each(players, function (player) {
 				// If choice is the truth, give 1000 points to that player
 				// and add that player in its list of choosers
@@ -176,30 +177,29 @@ function onChoice(socket, questions) {
 						// Do not give points to a player who picks his own answer
 						if (potentialAuthor.name !== player.name && potentialAuthor.lastAnswer === player.lastChoice) {
 							potentialAuthor.score += POINTS_FOR_LIE;
-							
 						}
 					});
 					// and add this player to the list of choosers of that choice
 					resultsMap[player.lastChoice].choosedBy.push(player.name);
 				}
 			});
-			
+
 			resultsMap = removeUnpickedChoices(resultsMap, truth);
 			var results = placeResultsIntoArray(resultsMap, truth);
-			
+
 			sendResultsOneByOne(0, results, function () {
-				console.log(scoresArray());
+				log(scoresArray());
 				questionIndex++;
 				var isFinal = (questionIndex === questions.length);
 				io.emit('scores', scoresArray(), isFinal);
-				
+
 				var scoreCooldownTimer = setTimeout(function () {
 					if (questionIndex < questions.length) {
-						console.log('Sending next question');
+						log('Sending next question');
 						resetAnswers();
 						io.emit('question', questions[questionIndex].question, questionIndex + 1, questions.length);
 					} else {
-						console.log('Game finished, no more questions');
+						log('Game finished, no more questions');
 						gameEnded = true;
 					}
 				}, TIME_AFTER_SCORES);
@@ -224,14 +224,14 @@ function shuffle(a) {
 
 function sendResultsOneByOne(index, results, callback) {
 	if (index === results.length) {
-		console.log('No more results to send');
+		log('No more results to send');
 		callback();
 		return;
 	}
-	
-	console.log('Sending result : ' + JSON.stringify(results[index]));
+
+	log('Sending result : ' + JSON.stringify(results[index]));
 	io.emit('result', results[index]);
-	
+
 	resultCooldownTimer = setTimeout(function () {
 		sendResultsOneByOne(index + 1, results, callback);
 	}, TIME_BETWEEN_RESULTS);
@@ -251,7 +251,7 @@ function placeResultsIntoArray(resultsMap, truth) {
 	// Insert truth at the end
 	results.push({
 		choice: truth,
-		authors: ['TRUTH'], 
+		authors: ['TRUTH'],
 		choosedBy: resultsMap[truth].choosedBy
 	});
 	return results;
@@ -273,7 +273,7 @@ function getResult(resultsMap, player) {
 		result.authors.push(player.name);
 		return result;
 	}
-	
+
 	return {authors: [player.name], choosedBy:[]};
 }
 
@@ -289,16 +289,16 @@ function scoresArray() {
 function onDisconnect(socket) {
 	return function () {
 		if (players[socket.id]) {
-			console.log('Player [' + players[socket.id].name + '] has left');
+			log('Player [' + players[socket.id].name + '] has left');
 			gameStarted = false;
 			if (countdownObject.timer && !gameEnded) {
 				clearGameTimers();
 				gameEnded = true;
 				io.emit('quit', players[socket.id].name);
 			}
-			
+
 			delete players[socket.id];
-			
+
 			if (!gameStarted && !gameEnded) {
 				var names = _.map(players, function (player) {
 					return player.name;
@@ -336,7 +336,7 @@ function computeChoices(truth) {
 	var answers = _.map(players, function (player) {
 		return player.lastAnswer;
 	});
-	
+
 	return _.uniq(answers).concat([truth]);
 }
 
@@ -353,19 +353,19 @@ function hasEveryPlayerAnswered() {
 }
 
 function countdown(countdownObject, seconds, callback) {
-	console.log('starting in ' + seconds + ' seconds');
+	log('starting in ' + seconds + ' seconds');
 	io.emit('starting', seconds);
 	if (seconds === 0) {
 		countdownObject.timer = null;
 		callback();
 		return;
 	}
-	
+
 	countdownObject.timer = setTimeout(function () {
 		countdown(countdownObject, seconds - 1, callback);
 	}, 1000);
 }
 
 http.listen(PORT, function () {
-	console.log('Question game server listening on port ' + PORT);
+	log('Question game server listening on port ' + PORT);
 });
