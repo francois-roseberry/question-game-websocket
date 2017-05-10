0e6277dff1934eede6d7c92d91a3a04dcca3c1b7
francois-roseberry <f_roseberry@live.fr>
2017-05-10 08:30:46 -0400

    Removed superfluous dependency

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -4,13 +4,13 @@ var karmaRunner = require('./build/karma_runner');
 var buildServices = require('./build/build-services.js');
 
 module.exports = function(grunt) {
-	
+
 	// Default task, do everything
 	grunt.registerTask('default', 'Build and test everything', ['lint', 'package', 'test']);
-	
+
 	// Quick build
 	grunt.registerTask('check', ['lint', 'package', 'runTest']);
-	
+
 	// Code quality
 	grunt.registerTask('lint', ['jshint', 'csslint']);
 	grunt.registerTask('test', ['service', 'runTest']);
@@ -19,17 +19,17 @@ module.exports = function(grunt) {
 	grunt.registerTask('karmaTest', 'Run the client unit test in all browser connected to Karma', function () {
 	  karmaRunner.runTests(grunt, this.async());
 	});
-	
+
 	// Packaging
 	grunt.registerTask('prepare', ['copy:html', 'copy:flattenSourceAndTest', 'copy:flattenSmokeTest']);
 	grunt.registerTask('copySource', ['copy:lib', 'copy:images', 'copy:sounds', 'copy:fonts', 'copy:data']);
 	grunt.registerTask('minify', ['cssmin']);
 	grunt.registerTask('package', ['prepare', 'browserify', 'concat', 'copySource', 'minify']);
-	
+
 	// Background services
 	grunt.registerTask('background', ['service', 'wait']);
 	grunt.registerTask('service', ['karmaServer', 'server', 'webdriver']);
-  
+
 	grunt.registerTask('server', function () {
 	  buildServices.startServer(this.async());
 	});
@@ -47,7 +47,7 @@ module.exports = function(grunt) {
 	  grunt.log.writeln("Run 'grunt check' to do a quick build while this process is running");
 	  this.async();
 	});
-	
+
 	grunt.initConfig({
 		jshint: {
 			options: {
@@ -71,7 +71,7 @@ module.exports = function(grunt) {
 				src: ['src/**/*.css']
 			}
 		},
-		
+
 		browserify: {
 			dist: {
 				files: {
@@ -84,15 +84,15 @@ module.exports = function(grunt) {
 			dependencies: {
 				src: [
 				    'bower_components/jquery/dist/jquery.min.js',
-					'bower_components/underscore/underscore.js',
-					'bower_components/d3/d3.min.js',
-					'bower_components/rxjs/dist/rx.all.min.js',
-					'bower_components/bootstrap/dist/js/bootstrap.min.js'
+						'bower_components/underscore/underscore.js',
+						'bower_components/d3/d3.min.js',
+						'bower_components/rxjs/dist/rx.all.min.js',
+						'bower_components/bootstrap/dist/js/bootstrap.min.js'
 				],
 				dest: 'target/dist/lib/dependencies.js'
 			}
 		},
-			
+
 		copy: {
 			flattenSourceAndTest: {
 				expand: true,
@@ -112,7 +112,7 @@ module.exports = function(grunt) {
 				filter: 'isFile',
 				flatten: true
 			},
-			
+
 			lib: {
 				expand: true,
 				cwd: 'bower_components',
@@ -123,13 +123,13 @@ module.exports = function(grunt) {
 				filter: 'isFile',
 				flatten: true
 			},
-			
+
 			html: {
 				src: ['src/static/index.html'],
 				dest: 'target/dist/index.html',
 				filter: 'isFile'
 			},
-			
+
 			images: {
                 expand: true,
                 cwd: 'src/images',
@@ -139,7 +139,7 @@ module.exports = function(grunt) {
                 dest: 'target/dist/images',
                 filter: 'isFile'
             },
-			
+
 			fonts: {
 				expand: true,
 				cwd: 'bower_components',
@@ -150,7 +150,7 @@ module.exports = function(grunt) {
 				filter: 'isFile',
 				flatten: true
 			},
-			
+
 			sounds: {
 				expand: true,
 				cwd: 'src/sounds',
@@ -161,7 +161,7 @@ module.exports = function(grunt) {
 				filter: 'isFile',
 				flatten: true
 			},
-			
+
 			data: {
 				expand: true,
 				src: [
@@ -172,7 +172,7 @@ module.exports = function(grunt) {
 				flatten: true
 			}
 		},
-		
+
 		mochaTest: {
 			smoke: {
 				options: {
@@ -205,7 +205,7 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-concat');
-	  
+
 	function exclude(filePattern) {
         return '!' + filePattern;
     }
