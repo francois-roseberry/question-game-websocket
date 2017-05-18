b75fb297aa1e91461d71dedc01ab0618946201b5
francois-roseberry <f_roseberry@live.fr>
2017-05-18 14:16:57 -0400

    Added a step to store test results in CI

---
+++ client/build/karma-ci.conf.js
@@ -0,0 +1,16 @@
+// Karma configuration
+"use strict";
+
+var baseConfig = require('./karma.conf.js');
+
+module.exports = function (config) {
+  baseConfig(config);
+
+    config.set({
+        reporters: ['spec', 'junit'],
+        junitReporter: {
+          outputDir: 'test-results'
+        },
+        singleRun: true
+    });
+};
