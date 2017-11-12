8b03016a060e411c521d374d426afeb8a363d246
francois-roseberry <f_roseberry@live.fr>
2017-11-12 10:49:35 -0500

    Implement the second unit test for shuffling array

--- server/test/unit/util.test.js
+++ server/test/unit/util.test.js
@@ -1,14 +1,22 @@
 var expect = require('chai').expect;
+var _ = require('underscore');
+
 var shuffle = require('../../src/util').shuffle;
 
 describe('Shuffling an array', () => {
   it('returns an array of the same length', () => {
-    const toShuffle = [1, 2, 3];
+    const toShuffle = [1, 2, 3, 4, 5];
     shuffle(toShuffle);
-    expect(toShuffle.length).to.eql(3);
+    expect(toShuffle.length).to.eql(5);
   });
 
   it('returns an array containing all the same elements', () => {
+    const toShuffle = [1, 2, 3, 4, 5];
+    const original = toShuffle.slice(0);
+    shuffle(toShuffle);
 
+    original.forEach(element => {
+      expect(_.contains(toShuffle, element)).to.eql(true);
+    });
   });
 });
