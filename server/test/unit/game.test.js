67f385bb6cc1adb7d601dc0af3d0d606defccce8
francois-roseberry <f_roseberry@live.fr>
2017-12-15 17:57:06 -0500

    Delete the unused game class

--- server/test/unit/game.test.js
+++ server/test/unit/game.test.js
@@ -7,11 +7,14 @@ const newPlayer = require('../../src/player').newPlayer;
 const gameStarted = require('./unit-test-utils').gameStarted;
 const gameStartedAnswered = require('./unit-test-utils').gameStartedAnswered;
 const gameStartedAnsweredChosen = require('./unit-test-utils').gameStartedAnsweredChosen;
+const gameStartedAnsweredChosenWithResultsOneByOne = require('./unit-test-utils').gameStartedAnsweredChosenWithResultsOneByOne;
 const gameCompleted = require('./unit-test-utils').gameCompleted;
 const contains = require('./unit-test-utils').contains;
 const assertResultsDoNotContainChoice = require('./unit-test-utils').assertResultsDoNotContainChoice;
 const QUESTIONS = require('./unit-test-utils').QUESTIONS;
 const CONFIG = require('./unit-test-utils').CONFIG;
+
+const TOLERANCE_MILLIS = 3;
 
 describe('A game', () => {
   it('can be created', () => {
@@ -83,11 +86,51 @@ describe('A game', () => {
       });
     });
 
-    it('sends the first question', done => {
-      gameStarted((game, player1, player2, question) => {
-        expect(_.isEqual(question, { index: 0, question: QUESTIONS[0].question})).to.eql(true);
+    it('send one starting event per second to start, then send the first question', done => {
+      const game = Game.create(CONFIG);
+      game.starting().take(5).toArray().subscribe(seconds => {
+        expect(seconds).to.eql([5,4,3,2,1]);
+        game.questions().take(1).timeout(CONFIG.millisecondsPerSecond + TOLERANCE_MILLIS).subscribe(question => {
+          expect(_.isEqual(question, { index: 0, question: QUESTIONS[0].question})).to.eql(true);
+          done();
+        });
+      });
+
+      game.start();
+    });
+
+    describe('when game is cancelled before countdown reaches zero', () => {
+      it('stops sending starting events', done => {
+        const game = Game.create(CONFIG);
+        game.starting().take(1).subscribe(() => {
+          done(new Error('should never be called'));
+        });
+        game.start();
+        game.cancel();
         done();
       });
+
+      it('sends the player list', done => {
+        const game = Game.create(CONFIG);
+        game.players().take(1).subscribe(players => {
+          done();
+        });
+        game.start();
+        game.cancel();
+      });
+    });
+
+    // TODO : game is cancelled automatically if player quits during countdown
+    // Works live, but somehow unit test fails
+    it.skip('should cancel start if player quits suddenly', done => {
+      const game = Game.create(CONFIG);
+      const player = newPlayer('bob');
+      game.addPlayer(player);
+      game.starting().take(5).toArray().subscribe(() => {
+        done(new Error('should never be called'));
+      });
+      game.start();
+      game.removePlayer(player.socketId);
     });
   });
 
@@ -140,6 +183,30 @@ describe('A game', () => {
 
   describe('when every player has chosen, sends the results', () => {
     const TRUTH = QUESTIONS[0].answer;
+
+    it('one by one', done => {
+      const TRUTH = QUESTIONS[0].answer;
+      const ANSWERS = { player1: TRUTH + '1', player2: TRUTH + '2' };
+      const CHOICES = { player1: TRUTH + '2', player2: TRUTH + '1' };
+      gameStartedAnsweredChosenWithResultsOneByOne(ANSWERS, CHOICES, (game, player1, player2, results) => {
+        expect(_.isEqual(results[0], { choice: ANSWERS.player1, authors: [player1.name], choosedBy: [player2.name] })).to.eql(true);
+        expect(_.isEqual(results[1], { choice: ANSWERS.player2, authors: [player2.name], choosedBy: [player1.name] })).to.eql(true);
+        done();
+      });
+    });
+
+    it('after sending scores, should wait for specified time before sending new question', done => {
+      const TRUTH = QUESTIONS[0].answer;
+      const ANSWERS = { player1: TRUTH + '1', player2: TRUTH + '1' };
+      const CHOICES = { player1: TRUTH + '1', player2: TRUTH };
+      gameStartedAnswered(ANSWERS, (game, player1, player2) => {
+        game.questions().take(1).subscribe(() => {
+          done();
+        });
+        game.choose(player1.socketId, CHOICES.player1);
+        game.choose(player2.socketId, CHOICES.player2);
+      });
+    });
 
     it('giving 1000 points to each player who found the truth', done => {
       const ANSWERS = { player1: TRUTH + '1', player2: TRUTH + '2' };
