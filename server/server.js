cd9377673a9bf1fb7f106fad685145615f15f699
francois-roseberry <f_roseberry@live.fr>
2017-12-16 12:41:33 -0500

    Remove dead code in server

--- server/server.js
+++ server/server.js
@@ -121,28 +121,6 @@ function onPlayerName(socket, game) {
 			log('User cannot join : ', error.message);
 			socket.emit('name response', false, error.message);
 		}
-
-		/*if (gameStarted) {
-			log('A user cannot join because game is aleady started');
-			socket.emit('name response', false, 'ALREADY_STARTED');
-			return;
-		}
-
-		var names = _.map(players, (player) => {
-			return player.name;
-		});
-		if (_.contains(names, name)) {
-			log('A user cannot join because name (' + name + ') already exists');
-			socket.emit('name response', false, 'EXISTING');
-		} else {
-			log('A user identified as [' + name + "]");
-			players[socket.id] = newPlayer(name);
-			socket.emit('name response', true);
-			var names = _.map(players, (player) => {
-				return player.name;
-			});
-			io.emit('players', names);
-		}*/
 	};
 }
 
@@ -154,18 +132,6 @@ function onStart(socket, game, questions) {
 		} catch (error) {
 			log('Could not start game : ', error.message);
 		}
-
-		/*if (!players[socket.id]) {
-			log('Game cannot be started by a player who is not logged in');
-			return;
-		}
-
-		log('Game started by [' + players[socket.id].name + ']');
-		countdown(countdownObject, secondsBeforeStart, () => {
-			gameStarted = true;
-			log('Game start, sending first question');
-			io.emit('question', questions[0].question, 1, questions.length);
-		});*/
 	};
 }
 
@@ -176,22 +142,6 @@ function onCancel(socket, game) {
 			log('Game start cancelled by [' + game.playerName(socket.id) + ']');
 			socket.emit('cancelled');
 		}
-
-		/*if (!players[socket.id]) {
-			log('Game cannot be cancelled by a user who is not logged in');
-			return
-		}
-
-		if (countdownObject.timer) {
-			log('Game start cancelled by [' + players[socket.id].name + ']');
-			clearTimeout(countdownObject.timer);
-			countdownObject.timer = null;
-
-			var names = _.map(players, (player) => {
-				return player.name;
-			});
-			io.emit('players', names);
-		}*/
 	};
 }
 
@@ -205,25 +155,6 @@ function onAnswer(socket, game, questions) {
 			log('Could not answer : ', error.message);
 			socket.emit('answer response', false, error.message);
 		}
-		/*if (!players[socket.id]) {
-			log('Question cannot be answered by a user who is not logged in');
-			return;
-		}
-
-		var truth = questions[questionIndex].answer;
-		if (answer === truth) {
-			socket.emit('answer response', false, 'TRUTH');
-		} else {
-			log('Player [' + players[socket.id].name + '] has answered ' + answer);
-			players[socket.id].lastAnswer = answer;
-			socket.emit('answer response', true);
-			if (hasEveryPlayerAnswered()) {
-				var choices = computeChoices(truth);
-				shuffle(choices);
-				log('Everybody has answered, sending choices : ' + JSON.stringify(choices));
-				io.emit('choices', choices);
-			}
-		}*/
 	};
 }
 
@@ -231,213 +162,14 @@ function onChoice(socket, game, questions) {
 	return choice => {
 		game.choose(socket.id, choice);
 		log('Player [' + game.playerName(socket.id) + '] has choosen ' + choice);
-
-		/*if (!players[socket.id]) {
-			log('A choice cannot be made by a player who is not logged in');
-			return;
-		}
-
-		log('Player [' + players[socket.id].name + '] has choosen ' + choice);
-		players[socket.id].lastChoice = choice;
-		if (hasEveryPlayerChosen()) {
-			log('Everybody has chosen, computing scores');
-
-			var truth = questions[questionIndex].answer;
-			var resultsMap = {};
-			resultsMap[truth] = { authors: ['TRUTH'], choosedBy: []};
-
-			_.each(players, (player) => {
-				resultsMap[player.lastAnswer] = getResult(resultsMap, player);
-			});
-
-			_.each(players, (player) => {
-				// If choice is the truth, give 1000 points to that player
-				// and add that player in its list of choosers
-				if (player.lastChoice === truth) {
-					player.score += POINTS_FOR_TRUTH;
-					resultsMap[truth].choosedBy.push(player.name);
-				} else {
-					// Otherwise, find out who created the choice and give each author 500 points
-					_.each(players, (potentialAuthor) => {
-						// Do not give points to a player who picks his own answer
-						if (potentialAuthor.name !== player.name && potentialAuthor.lastAnswer === player.lastChoice) {
-							potentialAuthor.score += POINTS_FOR_LIE;
-						}
-					});
-					// and add this player to the list of choosers of that choice
-					resultsMap[player.lastChoice].choosedBy.push(player.name);
-				}
-			});
-
-			resultsMap = removeUnpickedChoices(resultsMap, truth);
-			var results = placeResultsIntoArray(resultsMap, truth);
-
-			sendResultsOneByOne(0, results, () => {
-				log(scoresArray());
-				questionIndex++;
-				var isFinal = (questionIndex === questions.length);
-				io.emit('scores', scoresArray(), isFinal);
-
-				var scoreCooldownTimer = setTimeout(() => {
-					if (questionIndex < questions.length) {
-						log('Sending next question');
-						resetAnswers();
-						io.emit('question', questions[questionIndex].question, questionIndex + 1, questions.length);
-					} else {
-						log('Game finished, no more questions');
-						gameEnded = true;
-					}
-				}, TIME_AFTER_SCORES);
-			});
-		}*/
 	};
 }
-
-/*function sendResultsOneByOne(index, results, callback) {
-	if (index === results.length) {
-		log('No more results to send');
-		callback();
-		return;
-	}
-
-	log('Sending result : ' + JSON.stringify(results[index]));
-	io.emit('result', results[index]);
-
-	resultCooldownTimer = setTimeout(() => {
-		sendResultsOneByOne(index + 1, results, callback);
-	}, TIME_BETWEEN_RESULTS);
-}
-
-function placeResultsIntoArray(resultsMap, truth) {
-	var results = [];
-	_.each(_.keys(resultsMap), (choice) => {
-		if (resultsMap[choice].authors[0] !== 'TRUTH') {
-			results.push({
-				choice: choice,
-				authors: resultsMap[choice].authors,
-				choosedBy: resultsMap[choice].choosedBy
-			});
-		}
-	});
-	// Insert truth at the end
-	results.push({
-		choice: truth,
-		authors: ['TRUTH'],
-		choosedBy: resultsMap[truth].choosedBy
-	});
-	return results;
-}
-
-function removeUnpickedChoices(resultsMap, truth) {
-	var newResultsMap = {};
-	_.each(_.keys(resultsMap), (choice) => {
-		if (choice === truth || (choice !== truth && resultsMap[choice].choosedBy.length > 0)) {
-			newResultsMap[choice] = resultsMap[choice];
-		}
-	});
-	return newResultsMap;
-}
-
-function getResult(resultsMap, player) {
-	if (resultsMap[player.lastAnswer]) {
-		var result = resultsMap[player.lastAnswer];
-		result.authors.push(player.name);
-		return result;
-	}
-
-	return {authors: [player.name], choosedBy:[]};
-}
-
-function scoresArray() {
-	return _.map(players, (player) => {
-		return {
-			name: player.name,
-			score: player.score
-		};
-	});
-}*/
 
 function onDisconnect(socket, game) {
 	return () => {
 		game.removePlayer(socket.id);
-
-		/*if (players[socket.id]) {
-			log('Player [' + players[socket.id].name + '] has left');
-			gameStarted = false;
-			if (countdownObject.timer && !gameEnded) {
-				clearGameTimers();
-				gameEnded = true;
-				io.emit('quit', players[socket.id].name);
-			}
-
-			delete players[socket.id];
-
-			if (!gameStarted && !gameEnded) {
-				var names = _.map(players, (player) => {
-					return player.name;
-				});
-				io.emit('players', names);
-			}
-		}*/
 	};
 }
-
-
-/*function clearGameTimers() {
-	if (countdownObject.timer) {
-		clearTimeout(countdownObject.timer);
-		countdownObject.timer = null;
-	}
-	if (resultCooldownTimer) {
-		clearTimeout(resultCooldownTimer);
-		resultCooldownTimer = null;
-	}
-	if (scoreCooldownTimer) {
-		clearTimeout(scoreCooldownTimer);
-		scoreCooldownTimer = null;
-	}
-}*/
-
-/*function resetAnswers() {
-	_.each(players, (player) => {
-		player.lastAnswer = null;
-		player.lastChoice = null;
-	});
-}
-
-function computeChoices(truth) {
-	var answers = _.map(players, (player) => {
-		return player.lastAnswer;
-	});
-
-	return _.uniq(answers).concat([truth]);
-}
-
-function hasEveryPlayerChosen() {
-	return _.every(players, (player) => {
-		return player.lastChoice != null;
-	});
-}
-
-function hasEveryPlayerAnswered() {
-	return _.every(players, (player) => {
-		return player.lastAnswer != null;
-	});
-}
-
-function countdown(countdownObject, seconds, callback) {
-	log('starting in ' + seconds + ' seconds');
-	io.emit('starting', seconds);
-	if (seconds === 0) {
-		countdownObject.timer = null;
-		callback();
-		return;
-	}
-
-	countdownObject.timer = setTimeout(() => {
-		countdown(countdownObject, seconds - 1, callback);
-	}, 1000);
-}*/
 
 http.listen(PORT, () => {
 	log('Question game server listening on port ' + PORT);
